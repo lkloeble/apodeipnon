bdb2ed2a725f63b38d775506b5d9aea04e4e46e5
Laurent.kloeble <lkloeble@gmail.com>
2018-05-27 17:26:48 +0200

    first test for executable jar

---
+++ src/main/java/org/orthodoxmusic/apodeipnon/HelloWorldApplication.java
@@ -0,0 +1,19 @@
+package org.orthodoxmusic.apodeipnon;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class HelloWorldApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Label label = new Label("Apodeipnon");
+        label.setAlignment(Pos.CENTER);
+        primaryStage.setScene(new Scene(label, 300, 250));
+        primaryStage.setTitle("Apodeipnon Application");
+        primaryStage.show();
+    }
+}
