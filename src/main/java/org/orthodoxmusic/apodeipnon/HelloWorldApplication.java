a0d59aa768f2a1e94d238f94ec7e2b5c82139cd8
Laurent.kloeble <lkloeble@gmail.com>
2018-06-09 18:13:23 +0200

    first update to check remote work wth Cantauque

--- src/main/java/org/orthodoxmusic/apodeipnon/HelloWorldApplication.java
+++ src/main/java/org/orthodoxmusic/apodeipnon/HelloWorldApplication.java
@@ -10,7 +10,7 @@ public class HelloWorldApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Label label = new Label("Apodeipnon");
+        Label label = new Label("Apodeipnon mise à jour réussie");
         label.setAlignment(Pos.CENTER);
         primaryStage.setScene(new Scene(label, 300, 250));
         primaryStage.setTitle("Apodeipnon Application");
