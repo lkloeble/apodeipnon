9ed247e52a06e04ec03964b765a17567d61b8633
Laurent.kloeble <lkloeble@gmail.com>
2018-07-12 20:54:22 +0200

    apostrophos and oligon draw on the same line

--- src/main/java/org/orthodoxmusic/apodeipnon/HelloWorldApplication.java
+++ src/main/java/org/orthodoxmusic/apodeipnon/HelloWorldApplication.java
@@ -58,6 +58,7 @@ public class HelloWorldApplication extends Application implements EventHandler<K
     public void drawIson() {
         System.out.println("drawIson");
         SVGPath svgPath = new SVGPath();
+        //svgPath.setContent("M390 199 c-229 -10 -314 -30 -340 -78 -24 -46 0 -82 41 -61 23 13 24 26 4 34 -27 10 -16 37 21 52 30 13 68 14 223 8 182 -6 187 -6 214 16 28 23 36 41 15 38 -7 0 -87 -5 -178 -9z");
         svgPath.setContent("M390 199 c-229 -10 -314 -30 -340 -78 -24 -46 0 -82 41 -61 23 13 24 26 4 34 -27 10 -16 37 21 52 30 13 68 14 223 8 182 -6 187 -6 214 16 28 23 36 41 15 38 -7 0 -87 -5 -178 -9z");
         svgPath.setScaleX(0.1);
         svgPath.setScaleY(0.1);
@@ -69,22 +70,24 @@ public class HelloWorldApplication extends Application implements EventHandler<K
     public void drawOligon() {
         System.out.println("drawOligon");
         SVGPath svgPath = new SVGPath();
-        svgPath.setContent("M334 180 c-220 -5 -244 -7 -263 -24 -12 -11 -21 -22 -21 -25 0 -3 110 -4 245 -1 236 5 245 6 267 27 12 12 20 24 17 25 -2 2 -113 1 -245 -2z");
+        //svgPath.setContent("M334 180 c-220 -5 -244 -7 -263 -24 -12 -11 -21 -22 -21 -25 0 -3 110 -4 245 -1 236 5 245 6 267 27 12 12 20 24 17 25 -2 2 -113 1 -245 -2z");
+        svgPath.setContent("M320 160 c-220 -5 -244 -7 -263 -24 -12 -11 -21 -22 -21 -25 0 -3 110 -4 245 -1 236 5 245 6 267 27 12 12 20 24 17 25 -2 2 -113 1 -245 -2z");
         svgPath.setScaleX(0.1);
         svgPath.setScaleY(0.1);
         svgPath.setTranslateX(lastXIsonCoordonate);
-        lastXIsonCoordonate += 80;
+        lastXIsonCoordonate += 60;
         group.getChildren().addAll(svgPath);
     }
 
     public void drawApostrophos() {
         System.out.println("drawApostrophos");
         SVGPath svgPath = new SVGPath();
-        svgPath.setContent("M157 183 c-4 -3 2 -12 13 -20 11 -8 20 -25 20 -39 0 -30 -44 -52 -121 -61 -52 -6 -68 -23 -21 -23 43 0 139 26 167 44 14 9 31 29 38 43 10 22 8 28 -12 44 -23 19 -71 26 -84 12z");
+        //svgPath.setContent("M157 183 c-4 -3 2 -12 13 -20 11 -8 20 -25 20 -39 0 -30 -44 -52 -121 -61 -52 -6 -68 -23 -21 -23 43 0 139 26 167 44 14 9 31 29 38 43 10 22 8 28 -12 44 -23 19 -71 26 -84 12z");
+        svgPath.setContent("M300 200 c-4 -3 2 -12 13 -20 11 -8 20 -25 20 -39 0 -30 -44 -52 -121 -61 -52 -6 -68 -23 -21 -23 43 0 139 26 167 44 14 9 31 29 38 43 10 22 8 28 -12 44 -23 19 -71 26 -84 12z");
         svgPath.setScaleX(0.1);
         svgPath.setScaleY(0.1);
         svgPath.setTranslateX(lastXIsonCoordonate);
-        lastXIsonCoordonate += 80;
+        lastXIsonCoordonate += 30;
         group.getChildren().addAll(svgPath);
     }
 
